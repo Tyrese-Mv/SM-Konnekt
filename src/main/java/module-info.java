260fda6c90ecc1f64af49322dcaece50a8184bf2
Tyrese-Mv <musawenkosi106@gmail.com>
2024-08-29 07:46:27 +0200

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module org.tyresemv.smkonnect {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens org.tyresemv.smkonnect to javafx.fxml;
+    exports org.tyresemv.smkonnect;
+}
