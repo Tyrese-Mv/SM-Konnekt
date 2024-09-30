15862f19526ba8e4c6d6789ba3138a06e5c36269
Tyrese-Mv <musawenkosi106@gmail.com>
2024-09-30 20:13:00 +0200

    added views associated with authentication

--- src/main/java/org/tyresemv/smkonnect/TwitterApplication.java
+++ src/main/java/org/tyresemv/smkonnect/TwitterApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.tyresemv.smkonnect.controllers.TwitterController;
 
 public class TwitterApplication extends Application {
     @Override
