458b5c913cd41dfb9323b940641ad50690198e8a
Tyrese-Mv <musawenkosi106@gmail.com>
2024-10-01 16:44:34 +0200

    an attempt to make view switching more efficient by not repeating myself

---
+++ src/main/java/org/tyresemv/smkonnect/controllers/SceneSwitcher.java
@@ -0,0 +1,16 @@
+package org.tyresemv.smkonnect.controllers;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class SceneSwitcher {
+//    public static void switchTo(String view, Object object) throws IOException {
+//        Parent root = FXMLLoader.load(Objects.requireNonNull(object.getClass().getResource(view)));
+//        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+//    }
+}
